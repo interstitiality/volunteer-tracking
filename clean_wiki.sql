ccb144929868a1391eac3eabf633b18eccd86c64
andyman <akane@freegeekseattle.org>
2014-05-26 07:15:26 -0700

    initial commit, files provided by ashenphoenix

---
+++ clean_wiki.sql
@@ -0,0 +1,8 @@
+
+DROP TABLE program;
+DROP TABLE free_geek_deets;
+DROP TABLE location;
+DROP TABLE emergency_contact;
+DROP TABLE reason;
+ALTER TABLE user DROP COLUMN user_dob;
+ALTER TABLE user DROP COLUMN user_phone;
